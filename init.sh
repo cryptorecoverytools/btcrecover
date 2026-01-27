873df7216b834f09566410dc3fad34188812c525
root <root@test.test>
2026-01-27 23:09:43 +0000

    added init script

---
+++ init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "INSTALLING ALL DEPENDENCIES..."
+
+apt update
+apt install wget ufw crunch pv python3-dev python3-pip cargo autoconf build-essential libtool swig pkg-config
+apt remove python3-urllib3
+
+echo "DOWNLOADING BTCRECOVER REPOSITORY..."
+
+wget https://github.com/cryptorecoverytools/btcrecover/archive/refs/heads/main.zip
+unzip main.zip
+cd ./btcrecover-main
+
+echo "INSTALLING THE PYTHON ENVIRONMENT..."
+
+ln -s /usr/bin/python3 /usr/local/bin/python
+export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
+pip3 install -r requirements-full.txt --break-system-packages
+
+echo "PREPARING FIREWALL RULES..."
+
+ufw disable>/dev/null
+ufw --force reset>/dev/null
+ufw default deny incoming
+ufw default deny outgoing
+ufw allow in 22/tcp>/dev/null
+ufw allow out 22/tcp>/dev/null
+ufw allow out 53/udp>/dev/null
+ufw enable
+
+echo "TO TEMPORARILY DISABLE THE FIREWALL, ENTER: ufw disable"
+echo ""
+echo "TO START THE RECOVERY PROCESS, PLEASE RUN THE FOLLOWING SCRIPT: generate.sh"
