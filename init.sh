b7eea3765307a555b6520b46182ab539b1cb7699
root <root@test.test>
2026-01-29 23:41:01 +0000

    deps fix

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ echo ""
 
 apt update
 apt -y install wget unzip ufw crunch pv python3-pip python3-dev pkg-config build-essential libssl-dev libffi-dev autoconf libtool swig
-apt -y remove python3-urllib3 python3-cryptography python3-idna
+apt -y remove python3-urllib3 python3-cryptography python3-idna python3-cffi-backend
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 . "$HOME/.cargo/env"
 #apt -y install rustup;rustup update stable
@@ -26,8 +26,9 @@ echo ""
 ln -s /usr/bin/python3 /usr/local/bin/python 2>/dev/null
 export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
 export PIP_BREAK_SYSTEM_PACKAGES=1
-pip3 install maturin
-pip3 install -r requirements-full.txt
+export PIP_ROOT_USER_ACTION=ignore
+pip3 install maturin cffi
+pip3 install -r requirements.txt
 apt -y install python3-pyopencl
 
 echo ""
